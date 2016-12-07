362ce720753abfc69ca90924834bf452fcc55692
Yami Medina <yamilethmedina1@gmail.com>
2016-12-07 15:43:22 -0500

    Changes to be committed: modified:   app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token, if: -> { action_name == 'create' }
 
   after_action :flash_to_headers
 

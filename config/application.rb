435138af0a661b141be9b35c8d173b5444128fa3
Yami Medina <yamilethmedina1@gmail.com>
2016-12-07 14:41:45 -0500

    edited application.rb with a potential fix to CSRF login problems: https://github.com/sleepepi/patstrial.org/issues/61

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,10 @@ module Logan
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = ENV['TIME_ZONE'] || "Central Time (US & Canada)"
+    config.time_zone = ENV['TIME_ZONE'] || "Eastern Time (US & Canada)"
+
+    # Prevent CSRF errors on WebKit browsers
+    skip_before_action :verify_authenticity_token, if: :devise_controller?
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true

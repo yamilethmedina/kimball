8b9cc30d4ce835cbc9e6b7ee752f4e791e3f3cd8
Yami Medina <yamilethmedina1@gmail.com>
2016-12-07 15:21:26 -0500

    potential CSRF fix inspired by this post: https://github.com/plataformatec/devise/issues/3102#issuecomment-48460387

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
+
   after_action :flash_to_headers
 
   # this is so that json requests don't redirect without a user

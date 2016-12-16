d06c7aa17002fb03aad3066bafd79e19f7efc33b
Yami Medina <yamilethmedina1@gmail.com>
2016-12-16 18:21:32 -0500

    edited session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Logan::Application.config.session_store :cookie_store, key: '_cutgroup_session', secure: (Rails.env.production? || Rails.env.staging?)
+# Logan::Application.config.session_store :cookie_store, key: '_cutgroup_session', secure: (Rails.env.production? || Rails.env.staging?)
+Rails.application.config.session_store :cookie_store, key: "_rails_session_#{Rails.env}", domain: :all

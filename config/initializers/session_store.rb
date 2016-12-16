c2c0a08e1ccb30cdd90477810cd83e9b70deca4d
Yami Medina <yamilethmedina1@gmail.com>
2016-12-16 18:33:58 -0500

    removed `domain: :all` from session_store. CSRF bug in Webkit browsers should be fixed!

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# Logan::Application.config.session_store :cookie_store, key: '_cutgroup_session', secure: (Rails.env.production? || Rails.env.staging?)
-Rails.application.config.session_store :cookie_store, key: "_rails_session_#{Rails.env}", domain: :all
+Rails.application.config.session_store :cookie_store, key: "_rails_session_#{Rails.env}"

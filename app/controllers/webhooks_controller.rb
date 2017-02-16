b672219dad117a98ae0b05ea7e186bef22a9ef5c
Yami Medina <yamilethmedina1@gmail.com>
2017-02-15 20:03:08 -0500

    testing accessing entries in hash

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -12,9 +12,11 @@ class WebhooksController < ApplicationController
     # put in database (with model? in something?)
 
     puts data
-    puts data.is_a?(Array)
+    puts data.is_a?(Hash) #it is not an array, should be a hash
 
-    # data['form_response']['answers']['field'].each do |id|
+    puts data["form_response"]["answers"]["field"]
+
+    # data["form_response"]["answers"]["field"].each do |id|
     #   print id
     # end
     # field_ids = JSON.parse(data.form_response.answers)['field'].map { |p| p['id'] }

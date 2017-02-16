1465938ae2463d9c14c92e0858a5813a90d65db9
Yami Medina <yamilethmedina1@gmail.com>
2017-02-16 15:14:56 -0500

    answers is an array

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -17,7 +17,7 @@ class WebhooksController < ApplicationController
 
 
     # puts data.first["form_response"]["answers"]["field"]
-    puts data.first["form_response"]["answers"]["field"].to_i
+    puts data["form_response"]["answers"][0]["field"]["id"].to_i
 
     # hash_of_params = JSON.load(request.params["form_response"]).to_hash
     # puts hash_of_params

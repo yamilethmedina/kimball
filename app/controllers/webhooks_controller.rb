283a247374003dec7e128aa49fc838a5d626c3a7
Yami Medina <yamilethmedina1@gmail.com>
2017-02-16 15:26:21 -0500

    changing labels to string

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -18,7 +18,7 @@ class WebhooksController < ApplicationController
 
     # puts data.first["form_response"]["answers"]["field"]
     puts data["form_response"]["answers"][0]["field"]["id"] # what languages do you speak?
-    puts data["form_response"]["answers"][0]["choices"]["labels"] # English, Spanish
+    puts data["form_response"]["answers"][0]["choices"]["labels"].to_s # English, Spanish
 
 
     # hash_of_params = JSON.load(request.params["form_response"]).to_hash

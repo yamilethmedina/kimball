496a6d66dd25993ed8ed728cb36ceb83e6e4279e
Yami Medina <yamilethmedina1@gmail.com>
2017-02-17 13:53:13 -0500

    working on saving individual field results from webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -19,7 +19,45 @@ class WebhooksController < ApplicationController
     # puts data.first["form_response"]["answers"]["field"]
     # puts data["form_response"]["answers"][0]["field"]["id"] 
     puts data["form_response"]["answers"][0] # randomized field
-    puts data["form_response"]["answers"]
+    @answers = data["form_response"]["answers"]
+    # puts answers
+    # answers.each do |item|
+    #   puts item 
+    # end
+
+    # fields = answers.find { |x| x["field"]["id"] == "41986482"}
+
+    languages = answers.find { |x| x["field"]["id"] == "41986482"}
+    unless languages["choices"]["other"].to_s.strip.empty?
+      languages["choices"]["labels"].push(languages["choices"]["other"])
+    end
+    puts language_list = languages["choices"]["labels"]
+
+
+    email_address = answers.find { |x| x["field"]["id"] == "41986489"}
+    puts email = email_address["email"]
+    # data.map
+
+    first = answers.find { |x| x["field"]["id"] == "41986475"}
+    puts first_name = first["text"]
+    
+    last = answers.find { |x| x["field"]["id"] == "41986476"}
+    puts last_name = last["text"]
+
+    addr = answers.find { |x| x["field"]["id"] == "41986477"}
+    puts address = addr["text"]
+
+    city_name = answers.find { |x| x["field"]["id"] == "41986478"}
+    puts city = city_name["text"]
+
+    state = "FL"
+
+    zip = answers.find { |x| x["field"]["id"] == "41986488"}
+    puts zip_code = zip["choice"]["label"]
+
+    mobile_phone = answers.find { |x| x["field"]["id"] == "41986479"}
+    puts phone_number = mobile_phone["text"]
+
 
 
     # hash_of_params = JSON.load(request.params["form_response"]).to_hash
@@ -33,7 +71,7 @@ class WebhooksController < ApplicationController
 
     # Person.create(:first_name => data.webhook.form_response.answers.map(@:text))
 
-    # @Person = Person.create(:first_name => params[:])
+    @Person = Person.create(:first_name => first_name, )
 
     render nothing: true
   end

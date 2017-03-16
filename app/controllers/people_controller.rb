38402750f95d22c3e63a28bd28c7275f2e77a08d
Yami Medina <yamilethmedina1@gmail.com>
2017-03-16 15:37:21 -0400

    moved google_map def back to private

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -255,10 +255,6 @@ class PeopleController < ApplicationController
     end
   end
 
-  def google_map(center)
-    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=570x200&zoom=14"
-  end
-
   private
 
     # Use callbacks to share common setup or constraints between actions.
@@ -291,6 +287,9 @@ class PeopleController < ApplicationController
       %w(asc desc).include?(params[:direction]) ? params[:direction] : 'desc'
     end
 
+    def google_map(center)
+      "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=570x200&zoom=14"
+    end
 
 end
 # rubocop:enable ClassLength
